6f467e2a89555ce69b8d3e9e7f57ce90f78ec510
Sara Dehghani <saradma@gmail.com>
2019-11-22 15:32:12 -0500

    everything together

--- client/src/component/ProductDetails/ProductDetails.js
+++ client/src/component/ProductDetails/ProductDetails.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import '../ProductDetails/ProductDetails.scss'
+import dataOne from '../../data/data-one.json'
+import dataTwo from '../../data/data-two.json';
+import dataThree from '../../data/data-three.json'
 
 class ProductDetails extends React.Component {
-    render (){
-        return(
+    data = [...dataOne, ...dataTwo, ...dataThree]
+    state = {
+        rest: {}
+    }
+
+    componentDidMount() {
+    
+       this.data.find(element => {
+            if (this.props.match.params.id == element.RestaurantID) {
+                this.setState({rest:element})
+            }
+
+        });
+
+
+
+    }
+    // this.state.rest.Address
+    render() {
+        return (
             <div className="detail">
-                <h1 className="detail__header">Restuarant 1</h1>
+                <h1 className="detail__header">{this.state.rest.RestaurantName}</h1>
                 <div className="detail__container">
                     <aside className="detail__side-menu">
                         <ul className="detail__side-menu__list">
-                            <li className="detail__side-menu__list-item">Rating</li>
-                            <li className="detail__side-menu__list-item">5 Stars</li>
+                            <li className="detail__side-menu__list-item">ranking</li>
+                            <li className="detail__side-menu__list-item">{this.state.rest.Aggregaterating}</li>
                             <li className="detail__side-menu__list-item">Cusine</li>
-                            <li className="detail__side-menu__list-item">Sushi</li>
+                            <li className="detail__side-menu__list-item">{this.state.rest.Cuisines}</li>
                             <li className="detail__side-menu__list-item">Availablilty</li>
                             <li className="detail__side-menu__list-item">Open</li>
                         </ul>
